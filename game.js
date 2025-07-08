5374a12e2579b616a880bac4218d5650b9787726
shohammallick25 <shohammallick528@gmail.com>
2025-07-08 23:22:52 +0600

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 var step = document.getElementsByClassName('astep');
 var rdice = document.getElementsByClassName('r-dice');
 var gdice = document.getElementsByClassName('g-dice');
