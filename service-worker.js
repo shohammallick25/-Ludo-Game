93a45b329ca1f77b3fad140227da1ec026f3fd7b
shohammallick25 <shohammallick528@gmail.com>
2025-07-11 00:58:59 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const cacheName = 'ludo-pwa-v1';
-const assets = [
+const CACHE_NAME = 'ludo-pwa-cache-v1';
+const urlsToCache = [
   '/',
   '/index.html',
   '/main.css',
@@ -8,23 +8,36 @@ const assets = [
   '/icon.png',
   '/dice.mp3',
   '/win.mp3',
-  '/step.mp3',
   '/killed.mp3',
-  '/inout.mp3'
+  '/step.mp3',
+  '/inout.mp3',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(assets);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting(); // Force install
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
